1ca8bd0adc9d130b330688e585a29eacc98baf98
Quincy Tromp <quincy.tromp@hotmail.com>
2022-11-01 21:28:05 +0100

    updated transaction_type column

--- query.sql
+++ query.sql
@@ -1,24 +1,23 @@
 set @start_dt = '2022-09-01';
 set @end_dt = '2022-09-30';
 
-select l.transact_type, l.label, sum(transact_amount)
+select l.transaction_type, l.label, sum(transact_amount)
 from transactions t
 right outer join labels l on l.label = t.label
-where l.transact_type = 'Income'
+where l.transaction_type = 'Income'
 and t.transact_dt between @start_dt and @end_dt
 group by t.label
 union
-select l.transact_type, l.label, sum(transact_amount)
+select l.transaction_type, l.label, sum(transact_amount)
 from transactions t
 right outer join labels l on l.label = t.label
-where l.transact_type = 'Expenses'
+where l.transaction_type = 'Expenses'
 and t.transact_dt between @start_dt and @end_dt
 group by t.label
 union
-select l.transact_type, l.label, sum(transact_amount)
+select l.transaction_type, l.label, sum(transact_amount)
 from transactions t
 right outer join labels l on l.label = t.label
-where l.transact_type = 'Savings'
+where l.transaction_type = 'Savings'
 and t.transact_dt between @start_dt and @end_dt
 group by t.label;
-
