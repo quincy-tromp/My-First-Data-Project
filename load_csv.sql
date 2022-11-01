6e3503622fbb1590b3fd6e13cb13e2e624fcb6aa
Quincy Tromp <quincy.tromp@hotmail.com>
2022-11-01 21:38:02 +0100

    Fixed Home path from '~' to 'Users'

--- load_csv.sql
+++ load_csv.sql
@@ -1,3 +1,3 @@
-LOAD DATA LOCAL INFILE '~/Downloads/Oct-2022.csv' INTO TABLE transactions
+LOAD DATA LOCAL INFILE 'Users/Downloads/Sep-2022.csv' INTO TABLE transactions
 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'  
 (@Transactiedatum,@Transactiebedrag,@Label) set transaction_dt=@Transactiedatum,amount=@Transactiebedrag,label=@Label;
