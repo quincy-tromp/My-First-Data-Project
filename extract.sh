17f43fb30dbb8791c26776cd4437cc99e9b23532
Quincy Tromp <quincy.tromp@hotmail.com>
2022-11-02 23:59:14 +0100

    extract, transform and load data

--- extract.sh
+++ extract.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
+#!/bin/zsh
 
 set -e
 
-FILENAME=`ls ~/Downloads/*.csv`
-FPATH=`readlink -f $FILENAME`
+FILENAME=`ls ~/Downloads/*.csv`&& FPATH=`readlink -f $FILENAME`
 python transform_csv.py $FPATH
+
+mysql --local-infile=1 -u${DB_USER} -p${DB_USER_PASSWORD} << here-doc
+USE PF_DB
+LOAD DATA LOCAL INFILE '$FPATH' INTO TABLE transactions
+FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'  
+(@Transactiedatum,@Transactiebedrag,@Label) set transaction_dt=@Transactiedatum,amount=@Transactiebedrag,label=@Label;
+commit;
+here-doc
