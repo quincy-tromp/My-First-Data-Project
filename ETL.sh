300111bf90289bc7e7b6fc75dbd169be8d8fa067
Quincy Tromp <quincy.tromp@hotmail.com>
2022-11-03 08:26:35 +0100

    added 2> /dev/null

--- ETL.sh
+++ ETL.sh
@@ -5,7 +5,7 @@
 FILENAME=`ls ~/Downloads/*.csv`&& FPATH=`readlink -f $FILENAME`
 python transform_csv.py $FPATH
 
-mysql --local-infile=1 -u${DB_USER} -p${DB_USER_PASSWORD} << here-doc
+mysql --local-infile=1 -u${DB_USER} -p${DB_USER_PASSWORD} 2> /dev/null << here-doc
 USE PF_DB
 LOAD DATA LOCAL INFILE '$FPATH' INTO TABLE transactions
 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'  
