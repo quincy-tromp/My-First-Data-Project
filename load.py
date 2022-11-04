cdc0972d0d868922aa2048ccf720b7d12abce153
Quincy Tromp <quincy.tromp@hotmail.com>
2022-11-04 14:22:05 +0100

    added try except else block, and trying to get env var into script

--- load.py
+++ load.py
@@ -1,33 +1,35 @@
 import extract as e
 import pymysql
+import os
 
 data = e.path_extracter()
 
-print('start loading')
+load_query = f"LOAD DATA LOCAL INFILE '{data}' INTO TABLE transactions \
+            FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' \
+            (@Transactiedatum,@Transactiebedrag,@Label) \
+            set transaction_dt=@Transactiedatum,amount=@Transactiebedrag,label=@Label;"
+
+print('connecting to db')
 try:
     mydb = pymysql.connect(
     host='localhost',
-    user='quincytromp',
-    password='je7z*B196A1LmGU',
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_USER_PASSWORD'),
     database='PF_DB',
     local_infile=True
     )
     mycursor = mydb.cursor()
 
-    data_load = f"LOAD DATA LOCAL INFILE '{data}' INTO TABLE transactions \
-            FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' \
-            (@Transactiedatum,@Transactiebedrag,@Label) \
-            set transaction_dt=@Transactiedatum,amount=@Transactiebedrag,label=@Label;"
-
-
-    mycursor.execute(data_load)
-    mycursor.close()
-    mydb.commit()
-    
-    print('end loading')
-
 except:
-    print('loading failed')
+    print('db connection failed')
 
-finally:
-    mydb.close()
+else:
+    print('start loading')
+    try:
+        mycursor.execute(load_query)
+        mycursor.close()
+        mydb.commit()
+        mydb.close()
+        print('end loading')
+    except:
+        print('loading failed') 
