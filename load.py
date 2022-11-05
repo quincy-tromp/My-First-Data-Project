336b5e58c555145d4ff6544b789a5e022426b70e
Quincy Tromp <quincy.tromp@hotmail.com>
2022-11-05 07:34:40 +0100

    beautify query

--- load.py
+++ load.py
@@ -4,17 +4,19 @@ import os
 
 data = e.path_extracter()
 
-load_data = f"LOAD DATA LOCAL INFILE '{data}' INTO TABLE transactions \
-            FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' \
-            (@Transactiedatum,@Transactiebedrag,@Label) \
-            set transaction_dt=@Transactiedatum,amount=@Transactiebedrag,label=@Label;"
+load_data = f'''
+LOAD DATA LOCAL INFILE '{data}' INTO TABLE transactions
+FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+(@Transactiedatum,@Transactiebedrag,@Label)
+set transaction_dt=@Transactiedatum,amount=@Transactiebedrag,label=@Label;
+'''
 
 try:
     mydb = pymysql.connect(
     host='localhost',
     user='quincytromp',
     password=os.getenv('MYSQL_PASSWORD'),
-    database='MF_DB',
+    database='test_DB',
     local_infile=True
     )
     mycursor = mydb.cursor()
@@ -23,12 +25,13 @@ except:
     print('db connection failed')
 
 else:
-    print('start loading data')
     try:
+        print('start loading data')
         mycursor.execute(load_data)
         mycursor.close()
         mydb.commit()
-        mydb.close()
         print('end load')
     except:
         print('loading failed') 
+    finally:
+        mydb.close()
