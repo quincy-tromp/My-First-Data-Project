44ad8f850f07474e31e3cd8c4e836546835cc3b3
Quincy Tromp <quincy.tromp@hotmail.com>
2022-11-05 21:37:32 +0100

    modified load.py switched MF_DB to test_DB

--- load.py
+++ load.py
@@ -16,7 +16,7 @@ try:
     host='localhost',
     user='quincytromp',
     password=os.getenv('MYSQL_PASSWORD'),
-    database='MF_DB',
+    database='test_DB',
     local_infile=True
     )
     mycursor = mydb.cursor()
